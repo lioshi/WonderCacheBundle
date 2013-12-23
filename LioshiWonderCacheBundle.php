d3a26214f6b69c74546ffee8816059ad6599aff1
lioshi <lionel.fenneteau@gmail.com>
2013-12-23 14:08:54 +0100

    Update and rename WonderCacheBundle.php to LioshiWonderCacheBundle.php

---
+++ LioshiWonderCacheBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lioshi\WonderCacheBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LioshiWonderCacheBundle extends Bundle
+{
+}
--- WonderCacheBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Wonder\CacheBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class WonderCacheBundle extends Bundle
-{
-}
