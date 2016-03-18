61f57f977ab865642466a72c789ecf823d441555
lioshi <lionel.fenneteau@gmail.com>
2016-03-18 15:29:40 +0100

    Update Memcached.php
    fixed: memcached list for docker env

--- Cache/Memcached.php
+++ Cache/Memcached.php
@@ -3,6 +3,8 @@ namespace Lioshi\WonderCacheBundle\Cache;
 
 use Lioshi\WonderCacheBundle\Cache;
 
+
+
 /**
  * Class used for extends Memcached
  * 
@@ -10,12 +12,14 @@ use Lioshi\WonderCacheBundle\Cache;
 class Memcached extends \Memcached  
 {  
     private $prefix;
+    private $memServers;
 
     public function __construct($servers, $options, $overrideoptions)   
     {  
         parent::__construct();  
             
         $this->prefix = "";
+        $this->memServers = $servers;
 
         // add servers         
         foreach ($servers as $server) {  
@@ -33,6 +37,8 @@ class Memcached extends \Memcached
             }  
         } 
         $this->addServers($servers); 
+
+        
 
         // set options ovverride in config.yml
         if (isset($overrideoptions)) {
@@ -70,7 +76,33 @@ class Memcached extends \Memcached
     public function getAllKeys(){
         $durations = $this->get(WonderCache::getDurationToCachedKeysFilename());
         $allKeys = array();
-        foreach (parent::getAllKeys() as $key) {
+
+        // not worked...
+        // $this->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+        // var_dump($this->getOption(Memcached::OPT_BINARY_PROTOCOL));
+        // var_dump(parent::getAllKeys());
+        // $allKeysFromMemached = parent::getAllKeys();
+        
+        // get keys from all servers  
+        $allKeysFromMemached =  array();
+        foreach ($this->memServers as $server) {  
+              
+            if (!isset($server['dsn'])) {  
+                throw new \LogicException("Memcached dsn must be defined for server");  
+            }  
+              
+            if (!isset($server['port'])) {  
+                throw new \LogicException("Memcached port must be defined for server");  
+            }  
+              
+            if (!isset($server['weight'])) {  
+                $server['weight'] = 0;  
+            }  
+
+            $allKeysFromMemached = array_merge($allKeysFromMemached, self::getMemcacheKeys($server['dsn'], $server['port']));
+        } 
+
+        foreach ($allKeysFromMemached as $key) {
             if (isset($durations[$key])){
                 $duration = $durations[$key];
             } else {
@@ -85,4 +117,79 @@ class Memcached extends \Memcached
 
         return $allKeys;
     }
+
+
+
+    function getMemcacheKeys($host = '127.0.0.1', $port = 11211){
+ 
+        $mem = @fsockopen($host, $port);
+        if($mem === FALSE) return -1;
+ 
+        // retrieve distinct slab
+        $r = @fwrite($mem, 'stats items' . chr(10));
+        if($r === FALSE) return -2;
+ 
+        $slab = array();
+        while( ($l = @fgets($mem, 1024)) !== FALSE){
+                // sortie ?
+                $l = trim($l);
+                if($l=='END') break;
+ 
+                $m = array();
+                // <STAT items:22:evicted_nonzero 0>
+                $r = preg_match('/^STAT\sitems\:(\d+)\:/', $l, $m);
+                if($r!=1) return -3;
+                $a_slab = $m[1];
+ 
+                if(!array_key_exists($a_slab, $slab)) $slab[$a_slab] = array();
+        }
+ 
+        // recuperer les items
+        reset($slab);
+        foreach($slab AS $a_slab_key => &$a_slab){
+                $r = @fwrite($mem, 'stats cachedump ' . $a_slab_key . ' 100' . chr(10));
+                if($r === FALSE) return -4;
+ 
+                while( ($l = @fgets($mem, 1024)) !== FALSE){
+                        // sortie ?
+                        $l = trim($l);
+                        if($l=='END') break;
+ 
+                        $m = array();
+                        // ITEM 42 [118 b; 1354717302 s]
+                        $r = preg_match('/^ITEM\s([^\s]+)\s/', $l, $m);
+                        if($r!=1) return -5;
+                        $a_key = $m[1];
+ 
+                        $a_slab[] = $a_key;
+                }
+        }
+ 
+        // close
+        @fclose($mem);
+        unset($mem);
+ 
+        // transform it;
+        $keys = array();
+        reset($slab);
+        foreach($slab AS &$a_slab){
+                reset($a_slab);
+                foreach($a_slab AS &$a_key) $keys[] = $a_key;
+        }
+        unset($slab);
+ 
+        return $keys;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 } 
